3bd45a54d1ab02fbe24452892942649ada8c2f2a
Maxuel Junior <maxuel.junior@stgen.com>
2024-07-21 10:43:14 -0300

    i'm being started a orcamentista page

---
+++ src/web/MTJM.WebApp.MVC/Controllers/Orcamentista/OrcamentistaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MTJM.WebApp.MVC.Services;
+
+namespace MTJM.WebApp.MVC.Controllers.Orcamentista;
+
+public class OrcamentistaController : Controller
+{
+    #region Properties
+    private readonly IRequestApiService _requestApiService;
+
+    #endregion
+
+    #region Constructor
+    public OrcamentistaController(IRequestApiService requestApiService)
+    {
+        _requestApiService = requestApiService;
+    }
+    #endregion
+
+    #region GET - Index
+    public IActionResult Index()
+    {
+        return View();
+    }
+    #endregion
+}
